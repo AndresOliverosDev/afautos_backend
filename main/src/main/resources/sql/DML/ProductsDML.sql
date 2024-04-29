8d3f9c63576ca50a6aab0af71b2738a705ac1a29
Andres Felipe Oliveros Anacona <andres.oliverosaf@gmail.com>
2024-04-29 17:34:30 -0500

    DML

--- main/src/main/resources/sql/DML/ProductsDML.sql
+++ main/src/main/resources/sql/DML/ProductsDML.sql
@@ -1,3 +1,4 @@
+use afautos;
 -- Insertar categoría de repuestos para automóviles
 INSERT INTO categories (cat_name, description) VALUES 
 ('Filtros', 'Filtros de aire, aceite, combustible, etc.'),
@@ -24,4 +25,4 @@ INSERT INTO products (prod_name, description, quantity, price, image_url, cat, b
 ('Líquido de frenos Motul DOT 4', 'Líquido de frenos de alto rendimiento para sistemas de frenos avanzados', 35, 50000.00, 'https://www.freepik.es/foto-gratis/composicion-diferentes-accesorios-coche_16137857.htm#query=repuestos%20auto&position=23&from_view=keyword&track=ais&uuid=6fe9bc2e-9a21-4506-b12c-49fb1bdd1b67', 2, 5),
 ('Filtro de combustible Mann Filter', 'Filtro de combustible de alta eficiencia para motores a gasolina y diésel', 55, 70000.00, 'https://www.freepik.es/foto-gratis/accesorios-coche-espacio-copia_16137815.htm#query=repuestos%20auto&position=24&from_view=keyword&track=ais&uuid=6fe9bc2e-9a21-4506-b12c-49fb1bdd1b67', 1, 3),
 ('Aceite de caja Motul Gear 75W-90', 'Aceite de caja de cambios sintético para transmisiones manuales y automáticas', 40, 120000.00, 'https://img.freepik.com/foto-gratis/accesorios-coche-espacio-copia_23-2149030410.jpg?size=626&ext=jpg&ga=GA1.1.289359447.1706918376&semt=ais', 4, 5),
-('Batería de arranque Bosch', 'Batería de arranque premium para vehículos', 15, 250000.00, 'https://img.freepik.com/foto-gratis/composicion
+('Batería de arranque Bosch', 'Batería de arranque premium para vehículos', 15, 250000.00, 'https://img.freepik.com/foto-gratis/composicion-diferentes-accesorios-coche_23-2149030435.jpg?size=626&ext=jpg&ga=GA1.1.289359447.1706918376&semt=ais',4,5)
