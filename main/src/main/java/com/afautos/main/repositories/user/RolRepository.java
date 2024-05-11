ccae5e7048b5e19e46c5b971ce04082845487087
Andres Felipe Oliveros Anacona <andres.oliverosaf@gmail.com>
2024-05-10 19:50:17 -0500

    New Start

--- main/src/main/java/com/afautos/main/repositories/user/RolRepository.java
+++
@@ -1,8 +0,0 @@
-package com.afautos.main.repositories.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.afautos.main.models.user.Rol;
-
-public interface RolRepository extends JpaRepository<Rol, Byte>{
-}
